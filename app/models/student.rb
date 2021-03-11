20a6e3e05c436ddf4c72f3d621c0f37aad481bb0
Frances Cao <frances.te.cao@gmail.com>
2021-03-10 21:43:50 -0500

    complete sso

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,11 +2,10 @@ class Student < ApplicationRecord
 
     def self.from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |student|
-            student.uid = auth.info.uid
+            # student.uid = auth.info.uid
             student.first_name = auth.info.first_name
             student.last_name = auth.info.last_name
             student.email = auth.info.email
-            student.password = SecureRandom.hex
         end
     end
 end
