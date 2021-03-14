22be3bd28284ed03d0e3640e60aed85e31d00ca7
Zhiyue-Li <zli148@ucsc.edu>
2021-03-14 16:17:02 -0700

    cleaned courses helper

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,23 +1,4 @@
 module CoursesHelper
-    # def translateDay(course)
-    #     if course.day.nil?
-    #         return ""
-    #     else
-    #         weekday = ["Sun ", "Sat ", "Fri ", "Thu ", "Wed ", "Tue ", "Mon "]
-    #         i = 6
-    #         result = ""
-    #         while i >= 0 do
-    #             minus = 2 ** i
-    #             if course.day / minus > 0 then
-    #                 result += weekday[i]
-    #                 course.day -= minus
-    #             end
-    #             i -= 1
-    #         end
-    #         return result[0...-1]
-    #     end
-    # end
-
     # def translateTime(t)
     #     if t.nil?
     #         return ""
