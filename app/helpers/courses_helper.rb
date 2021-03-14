33dabf4f9b9e04b3ce007a485ed156c1534bef4b
Frances Cao <frances.te.cao@gmail.com>
2021-03-14 15:57:59 -0400

    add simplecov coverage

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,28 +1,28 @@
 module CoursesHelper
-    def translateDay(course)
-        if course.day.nil?
-            return ""
-        else
-            weekday = ["Sun ", "Sat ", "Fri ", "Thu ", "Wed ", "Tue ", "Mon "]
-            i = 6
-            result = ""
-            while i >= 0 do
-                minus = 2 ** i
-                if course.day / minus > 0 then
-                    result += weekday[i]
-                    course.day -= minus
-                end
-                i -= 1
-            end
-            return result[0...-1]
-        end
-    end
+    # def translateDay(course)
+    #     if course.day.nil?
+    #         return ""
+    #     else
+    #         weekday = ["Sun ", "Sat ", "Fri ", "Thu ", "Wed ", "Tue ", "Mon "]
+    #         i = 6
+    #         result = ""
+    #         while i >= 0 do
+    #             minus = 2 ** i
+    #             if course.day / minus > 0 then
+    #                 result += weekday[i]
+    #                 course.day -= minus
+    #             end
+    #             i -= 1
+    #         end
+    #         return result[0...-1]
+    #     end
+    # end
 
-    def translateTime(t)
-        if t.nil?
-            return ""
-        else
-            return t.strftime("%I:%M %p")
-        end
-    end
+    # def translateTime(t)
+    #     if t.nil?
+    #         return ""
+    #     else
+    #         return t.strftime("%I:%M %p")
+    #     end
+    # end
 end
