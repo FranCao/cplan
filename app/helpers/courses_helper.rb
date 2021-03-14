987a1cbfd47ad04bcac76938664d9cb23bd271ba
Zhiyue-Li <zli148@ucsc.edu>
2021-03-13 17:23:27 -0800

    fixed courses bugs

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,16 +1,28 @@
 module CoursesHelper
     def translateDay(course)
-        weekday = ["Sun ", "Sat ", "Fri ", "Thu ", "Wed ", "Tue ", "Mon "]
-        i = 6
-        result = ""
-        while i >= 0 do
-            minus = 2 ** i
-            if course.day / minus > 0 then
-                result += weekday[i]
-                course.day -= minus
+        if course.day.nil?
+            return ""
+        else
+            weekday = ["Sun ", "Sat ", "Fri ", "Thu ", "Wed ", "Tue ", "Mon "]
+            i = 6
+            result = ""
+            while i >= 0 do
+                minus = 2 ** i
+                if course.day / minus > 0 then
+                    result += weekday[i]
+                    course.day -= minus
+                end
+                i -= 1
             end
-            i -= 1
+            return result[0...-1]
         end
-        return result[0...-1]
+    end
+
+    def translateTime(t)
+        if t.nil?
+            return ""
+        else
+            return t.strftime("%I:%M %p")
+        end
     end
 end
