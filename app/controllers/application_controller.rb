d7f5d75e4583e596c5af2c6e7ddbc80ad4a59b46
Frances Cao <frances.te.cao@gmail.com>
2021-03-26 23:21:09 -0400

    add security for pages that require authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
   #     session[:student_id] = cookies[:stub_student_id] if cookies[:stub_student_id]
   #   end
   # end
+  include ApplicationHelper
+  protect_from_forgery with: :exception
 
 end
