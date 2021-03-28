a984e07d10003031dcc7a6f81bed6e221ea710e1
Zhiyue-Li <zli148@ucsc.edu>
2021-03-28 15:27:34 -0700

    rspec

--- spec/controllers/schedules_controller_spec.rb
+++ spec/controllers/schedules_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe SchedulesController, :type => :controller do
-	
+	describe 'index' do
+        it 'get all courses' do
+            get :index
+            expect(response).to render_template("index")
+        end
+    end
+
 	describe 'add' do
 		it "add to schedule" do
-			# @schedule_info = {student_id: "2", courses_offering_id: "1"}
-			# Schedule.stub(:add).with(@schedule_info)
 			post :add, params:{student_id: "2", courses_offering_id: "1"}
 		end
     end
