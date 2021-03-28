0c42d1f6a49192eeeb10a9232f798d23053e8bd2
Frances Cao <frances.te.cao@gmail.com>
2021-03-28 17:17:18 -0400

    add remove to schedule steps

--- features/step_definitions/schedule_steps.rb
+++ features/step_definitions/schedule_steps.rb
@@ -10,6 +10,10 @@ Then /I should see "Add"/ do
     expect(page).to have_link('Add', href: add_schedule_path)
 end
 
+Then /I should see "Remove"/ do
+    expect(page).to have_link('Remove', href: delete_schedule_path)
+end
+
 Given /the following courses_offerings exist/ do |courses_offerings_table|
     courses_offerings_table.hashes.each do |course|
         CoursesOffering.create course
