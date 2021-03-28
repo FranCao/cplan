8583b746e7265808e0f646d5d1b00d74ee4d8af8
Zhiyue-Li <zli148@ucsc.edu>
2021-03-28 15:58:13 -0700

    added spec for schedule controller

--- spec/controllers/schedules_controller_spec.rb
+++ spec/controllers/schedules_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe SchedulesController, :type => :controller do
+
 	describe 'index' do
         it 'get all courses' do
             get :index
@@ -11,11 +12,14 @@ describe SchedulesController, :type => :controller do
 	describe 'add' do
 		it "add to schedule" do
 			post :add, params:{student_id: "2", courses_offering_id: "1"}
+			expect(Schedule.where(student_id: "2", courses_offering_id: "1")).to be_truthy
 		end
     end
 
 	describe 'destroy' do
 		it "remove from schedule" do
+			post :destroy, params:{student_id: "2", courses_offering_id: "1"}
+			expect(Schedule.where(student_id: "2", courses_offering_id: "1").length).to eq(0)
 		end
 	end
 
