b3233e98a88bac70768ea3feac62e2ebb31ece4f
Zhiyue-Li <zli148@ucsc.edu>
2021-03-27 19:11:17 -0700

    Schedule

--- app/helpers/courses_offerings_helper.rb
+++
@@ -1,9 +0,0 @@
-module CoursesOfferingsHelper
-    def translateTime(t)
-        if t.nil?
-            return ""
-        else
-            return t.strftime("%I:%M")
-        end
-    end
-end
--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,5 @@
 module SchedulesHelper
+    def parseTime(t)
+        return t.strftime("%H:%M") 
+    end
 end
