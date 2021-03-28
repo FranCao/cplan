42004758680860293e603ea6c042bae2ffcf28bc
Frances Cao <frances.te.cao@gmail.com>
2021-03-28 00:22:11 -0400

    add course list to schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,14 @@
 class SchedulesController < ApplicationController
   def index
     @courses = CoursesOffering.where(semester: 'Summer')
+    @student = session[:student_id]
   end
+
+  def show
+		id = params[:id]
+		@student = Student.find(id)
+		if @student.graduation_year == nil || @student.graduation_year.nil?
+			redirect_to edit_student_path(@student)
+		end
+	end
 end
--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -2,4 +2,8 @@ module SchedulesHelper
     def parseTime(t)
         return t.strftime("%H:%M") 
     end
+
+    def parseTimePretty(t)
+        return t.strftime("%I:%M%p") 
+    end
 end
