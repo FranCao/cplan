7ff266cf41905a7a410f5d95387effddf6f7154b
unknown <yunboryan@gmail.com>
2021-04-05 00:41:22 -0400

    added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,59 @@ class StudentsController < ApplicationController
 		if @student.graduation_year == nil || @student.graduation_year.nil?
 			redirect_to edit_student_path(@student)
 		end
+		@taken_courses = @student.courses
+		@track_requirements = Track.find(@student.track_id).trackRequirements
+
+		@system_req =  {"name": "Breadth Requirement System Group", "satisfied": false, "courses_pending": [], "courses_completed": []}
+		@theory_req = {"name": "Breadth Requirement Theory Group", "satisfied": false, "courses_pending": [], "courses_completed": []}
+		@ai_req = {"name": "Breadth Requirement AI & Applications Group", "satisfied": false, "courses_pending": [], "courses_completed": []}
+		
+		@required_req = {"name": "Required Courses", "satisfied": false, "courses_pending": [], "courses_completed": []}
+		
+		@general_req = {"name": "General Elective", "satisfied": false, "courses_pending": [], "courses_completed": []}
+
+		@track_elective_req = {"name": "Track Elective", "satisfied": false, "courses_pending": [], "courses_completed": []}
+
+		@track_requirements.each do |requirement|
+			course = Course.find(requirement.course_id)
+			completed = @taken_courses.include?(course)
+			if requirement.is_required
+				if completed
+					@required_req[:courses_completed] << course
+				else
+					@required_req[:courses_pending] << course
+				end
+			elsif requirement.is_aiapplications_breadth_requirement
+				if completed
+					@ai_req[:courses_completed] << course
+				else
+					@ai_req[:courses_pending] << course
+				end
+			elsif requirement.is_systems_breadth_requirement
+				if completed
+					@system_req[:courses_completed] << course
+				else
+					@system_req[:courses_pending] << course
+				end
+			elsif requirement.is_theory_breadth_requirement
+				if completed
+					@theory_req[:courses_completed] << course
+				else
+					@theory_req[:courses_pending] << course
+				end
+			elsif requirement.is_general_elective
+				if completed
+					@track_elective_req[:courses_completed] << course
+				else
+					@track_elective_req[:courses_pending] << course
+				end
+			end
+		end
+
+		@breadth_req = {"name": "Breadth Requirements", "satisfied": false, "theory": @theory_req, "system": @system_req, "ai": @ai_req}
+
+		@track_req = {"breadth": @breadth_req, "required": @required_req, "general elective": @general_req, "track elective": @track_elective_req}
+
 	end
 
 	def create
