d349df76cb0cc7fadda144b35c4b67d788febb63
dumplinginsoda <suli94730@gmail.com>
2021-04-04 21:17:43 -0400

    db: add trackReq foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_164243) do
+ActiveRecord::Schema.define(version: 2021_04_05_011004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,5 +95,6 @@ ActiveRecord::Schema.define(version: 2021_03_28_164243) do
   end
 
   add_foreign_key "students", "tracks"
+  add_foreign_key "track_requirements", "courses"
   add_foreign_key "track_requirements", "tracks"
 end
