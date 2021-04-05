f2a0d0bb9bd835cc5c7d9fd424f798ff5cd35ead
unknown <yunboryan@gmail.com>
2021-04-04 21:27:16 -0400

    added taken_course

---
+++ db/migrate/20210402222732_create_taken_courses.rb
@@ -0,0 +1,10 @@
+class CreateTakenCourses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :taken_courses do |t|
+      t.integer :student_id
+      t.integer :course_id
+
+      t.timestamps
+    end
+  end
+end
