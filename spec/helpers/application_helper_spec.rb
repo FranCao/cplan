f0830256793eb7fba4f856196754ee95736f91e8
Frances Cao <frances.te.cao@gmail.com>
2021-04-19 18:00:17 -0400

    add boiler for helper spec, delete auto-gen admin_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -18,4 +18,21 @@ describe ApplicationHelper do
     #     end
     # end
 
+    describe 'is_admin' do
+        it 'redirects to the home page if user is not admin' do
+            delete :destroy
+            get :edit, params: {id: 1}
+            expect(flash[:notice]).to match(/Please login to view this page/)
+            expect(helper.require_login).to redirect_to(home_path)
+        end
+    end
+
+    describe '#is_admin' do
+        it "should redirect to homepage if not logged in" do
+            session[:student_id].should be_nil
+            get :admin
+            expect(flash[:success]).to match(/Please login to view this page/)
+        end
+    end
+
 end
